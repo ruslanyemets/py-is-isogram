4d57e5206c765e5527be48fc50ec82c6c899677b
Ruslan <ruslanyemets@gmail.com>
2024-12-19 09:55:47 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_function_should_return_valid_boolean_value(
+        word: str,
+        expected_result: bool
+) -> None:
+    assert is_isogram(word) == expected_result
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        "bingo",
+    ]
+)
+def test_function_should_be_case_insensitive(word: str) -> None:
+    assert is_isogram(word.upper()) == is_isogram(word.lower())
